4b1629d288d6b5043c8e8a27888b334a1e38feae
Kevin Martins da Silva <kevinmar704@gmail.com>
2025-08-05 08:50:59 -0300

    dia 05/08/25 att de autenticação com o google

---
+++ firebaseauth.js
@@ -0,0 +1,82 @@
+//importa as funçoes necessarias do firebase
+import { initializeApp } from "";
+import { getAuth, GoogleProvider, signInWithPopup, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "";
+import { getFirestore, setDoc, doc } from "";
+
+//Configs do FB
+
+
+//Inicia o FB
+const app = initializeApp(firebaseConfig);
+
+//func para exibir mendagens temporárias na interface
+function showMessage(message, divId) {
+
+    var messageDiv = document.getElementById(divId);
+    messageDiv.style.display = "block";
+    messageDiv.innerHTML.display = message;
+    messageDiv.style.opacity = 1;
+
+    setTimeout(function(){
+        messageDiv.style.opacity = 0;
+    }, 5000); //a m,esagem desaparece depois de 5 segundos
+
+}
+
+//Lógica do cadastro de novos usuários
+const signUp = document.getElementById('submitSignUp')
+signUp.addEventListener('click', (event) => {
+
+    event.preventDefault(); // previne o comportamento padrão do botao
+
+    //adicionar os dados do forms de cadastro
+    const email = document.getElementById('rEmail').value;
+    const password = document.getElementById('rPassword').value;
+    const firstName = document.getElementById('fName').value;
+    const lastName = document.getElementById('lName').value;
+
+    const auth = getAuth(); //Configura o serviço de autenticaçao
+    const db = getFirestore(); // Conecta ao firestore
+
+    //Cria uma conta com email e senha
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user; //Usuário autenticado
+        const userData = { email, firstName, lastName }; //dados do usuario para salvar
+
+        showMessage('Conta criada com sucesso', 'signUp') //Exibe uma mensagem de sucesso
+
+        //Salva os dados do usuário no firestore
+        const docRef = doc(db, "users", user.uid);
+        setDoc(docRef, userData)
+        .then(() => {
+            window.location.href = 'index.html'; //Redireciona para a pagina de login após o cadastro
+        })
+        .catch((error) => {
+            console.error("Error writing document", error);
+        });
+    })
+    .catch((error) => {
+            console.errorCode = error.code;
+            if (errorCode == 'auth/email-already') {
+                showMessage('Endereço de email já existe', 'signUpMessage');
+            } else {
+                showMessage('Não é possível criar usuário', 'signUpMessage');
+            }
+        });
+});
+
+//Lógica de login de3 usuários existentes
+const signIn = document.getElementById('submitSigIn');
+signIn.addEventListener('clcik', (event) => {
+
+    event.preventDefault(); //Previne o comportamento do botão
+
+    //adicioa os dados forms de login
+    const email = document.getElementById('rEmail').value;
+    const password = document.getElementById('rPassword').value;
+    const auth = getAuth(); //configura o serviço de autenticação
+
+    //realiza o login com email e senha
+
+})
---
+++ script.js
@@ -0,0 +1,21 @@
+//Obtêm os elementos de botão e formularios de login / cadastro
+const signUpButton = document.getElementById('signUpButton');
+const signInButton = document.getElementById('signInButton');
+const signUpForm = document.getElementById('signUpForm');
+const signInForm = document.getElementById('signInForm');
+
+//quando o botão de cadastro é clicado esconde o formulário de login e mostra o cadastro
+signUpButton.addEventListener('click', function() {
+
+    signInForm.style.display = "none";
+    signUpForm.style.display = "block";
+
+});
+
+//quando o botao de login é clicado, esconde o forms de cadastro e mostra o de login
+signInButton.addEventListener('click', function(){
+
+    signInForm.style.display = "block";
+    signUpForm.style.display = "none";
+
+})
